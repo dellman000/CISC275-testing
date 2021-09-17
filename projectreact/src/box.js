621bb1b74dd7aae51bdb561e28ebe7fb1f527175
Kendell Rennie <kenren@udel.edu>
2021-09-16 22:42:28 -0400

    added red and blue turn system

--- projectreact/src/box.js
+++ projectreact/src/box.js
@@ -13,26 +13,37 @@ const log=()=>  {
 
 const RepeatBox=()=> {
     let map= new Map(); 
+    let x = 1;
+    let countX=0;
+    let y = 3;
+    let countY=0;
     for(let i =1;i<10;i++){
-        map.set(i,"gray");
+      
+        map.set(i,"gray",[x,y]); 
     }
    const[colorStates,setColorStates]=useState(map);
+   const[turn,setTurn]=useState('red');
+
    //let color = 'gray'
   // let setcolor = () => color = 'red'
+
   const changecolor=(id)=>  {
       const newMap = new Map(colorStates)
-    if(newMap.get(id)=='gray' ){
+    if(newMap.get(id)=='gray' && turn=='red' ){
         newMap.set(id, 'red')
         setColorStates(newMap);
+        setTurn('blue');
     }
-    else{
-        newMap.set(id,'gray');
+    else if(newMap.get(id)=='gray'&& turn =='blue' ){
+        newMap.set(id, 'blue')
         setColorStates(newMap);
+        setTurn('red');
     }
-   
+    
   }
 
-const list = [];
+
+    const list = [];
     for(let i=1;i<10; i++){
         let TileButton = <div onClick = {() => changecolor(i)} style={{backgroundColor: colorStates.get(i)}} className="square" id={i}></div>
        list[i]=TileButton;
